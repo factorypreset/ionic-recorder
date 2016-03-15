07ea21f39d68142dc94b7100d88190fa35ea2089
Doron Tal <doron@tracktunes.org>
2016-03-15 07:57:50 -0400

    now showing selected nav menu item as loaded from DB

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,14 @@
 // Copyright (C) 2015, 2016 Tracktunes Inc
 
-import {App, IonicApp, Platform, Nav} from 'ionic-angular';
+import {App, IonicApp, Platform, Nav, NavController} from 'ionic-angular';
 import {Type, enableProdMode, ViewChild} from 'angular2/core';
 import {WebAudio} from './providers/web-audio/web-audio';
 import {LocalDB, DB_NAME, MAX_DB_INIT_TIME} from './providers/local-db/local-db';
 import {AppState} from './providers/app-state/app-state';
-import {RecordPage} from './pages/record/record';
-import {LibraryPage} from './pages/library/library';
 import {TabsPage} from './pages/tabs/tabs';
 
+
+const IMPOSSIBLE_TAB_INDEX = -1;
 
 @App({
     templateUrl: 'build/app.html',
@@ -18,18 +18,14 @@ import {TabsPage} from './pages/tabs/tabs';
     }
 })
 export class TracktunesApp {
-    // see the following link for how/why we use ViewChild and not
-    // NavController here. https://github.com/driftyco/ionic/issues/5543
-    @ViewChild(Nav) nav;
-    // private rootPage: Type = null;
-    private rootPage: Type = TabsPage;
-    // cause AppState and LocalDB singletons
-    // so that they are loaded as early as possible 
     private localDB: LocalDB = LocalDB.Instance;
     private appState: AppState = AppState.Instance;
-    private selectedTab: number = -1;
-    private recordPage: Type = RecordPage;
-    private libraryPage: Type = LibraryPage;
+    private rootPage: Type = TabsPage;
+
+    // make selectedTab not a real number so that it gets set
+    // by what we get from app state for the first time. we use
+    // it to ensure we're not updating app state on no change.
+    private selectedTab: number = IMPOSSIBLE_TAB_INDEX;
 
     constructor(
         private app: IonicApp,
@@ -37,12 +33,12 @@ export class TracktunesApp {
         console.log('constructor():TracktunesApp');
         // NB: you can delete the DB here to get rid of it easily in Firefox
 
-        this.platform.ready().then(() => {
-
-        });
+        // this.platform.ready().then(() => {
+        // });
     }
 
-    onPageWillEnter() {
+    ngOnInit() {
+        console.log('OPE');
         this.appState.getProperty('lastSelectedTab').subscribe(
             (tabIndex: number) => {
                 this.selectTab(tabIndex, false);
@@ -58,20 +54,20 @@ export class TracktunesApp {
             return;
         }
 
-        console.log('selectTab: ' + tabIndex);
+        let prevId: string = 'button' + this.selectedTab,
+            currentId: string = 'button' + tabIndex;
+
+        if (this.selectedTab !== IMPOSSIBLE_TAB_INDEX) {
+            document.getElementById(prevId).classList
+                .remove('button-selected');
+        }
+
+        document.getElementById(currentId).classList
+            .add('button-selected');
 
         this.selectedTab = tabIndex;
 
         this.app.getComponent('nav-tabs').select(tabIndex);
-
-        if (tabIndex === 0) {
-            console.log('setting to record page');
-            // this.nav.setRoot(RecordPage);
-        }
-        else if (tabIndex === 1) {
-            console.log('setting to library page');
-            // this.nav.setRoot(LibraryPage);
-        }
 
         if (updateAppState) {
             this.appState.updateProperty('lastSelectedTab', tabIndex)
