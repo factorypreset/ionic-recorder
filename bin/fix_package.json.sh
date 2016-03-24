798a9e52859b89d15a2e72dfd74b67d89a2be28c
Doron Tal <doron@tracktunes.org>
2016-03-24 01:42:55 -0400

    package upgrades sweep

--- bin/fix_package.json.sh
+++ bin/fix_package.json.sh
@@ -2,6 +2,9 @@
 
 # Copyright (C) 2015, 2016 Tracktunes Inc
 
+# Originally based on package.json at:
+#     https://github.com/lathonez/clicker/blob/master/package.json
+
 
 NLINES="`wc -l package.json | awk '{print $1}'`"
 NLINES_MINUS2="`echo $NLINES - 2 | bc`"
@@ -17,8 +20,14 @@ cat << EOF >> $TMPFILE
     "url": "https://github.com/tracktunes/ionic-recorder.git"
   },
   "scripts": {
+    "build": "gulp --gulpfile test/gulpfile.ts --cwd ./ ionic.build",
+    "e2e": "gulp --gulpfile test/gulpfile.ts --cwd ./ test.build.e2e && ./node_modules/protractor/bin/protractor test/protractor.conf.js",
+    "karma": "gulp --gulpfile test/gulpfile.ts --cwd ./ test.karma.debug",
+    "postinstall": "typings install",
     "start": "ionic serve --browser chromium-browser",
     "test": "find ./app -type f | xargs sed -i 's/[ \t]*$//' ; gulp --gulpfile test/gulpfile.ts --cwd ./ test"
+    "test.watch": "gulp --gulpfile test/gulpfile.ts --cwd ./ test.watch.build",
+    "webdriver-update": "webdriver-manager update"
   }
 }
 EOF
--- bin/recreate-app.sh
+++ bin/recreate-app.sh
@@ -2,6 +2,9 @@
 
 # Copyright (C) 2015, 2016 Tracktunes Inc
 
+# Originally based on package.json at:
+#     https://github.com/lathonez/clicker/blob/master/package.json
+
 # NOTE: make sure you have latest ionic cli installed
 
 APP_NAME="ionic-recorder"
@@ -41,28 +44,37 @@ cd $APP_NAME
 ./bin/fix_package.json.sh
 
 npm install --save-dev \
-    ionic-app-lib \
-    chalk \
-    del \
-    gulp \
-    gulp-load-plugins \
-    gulp-inline-ng2-template \
-    gulp-tap gulp-tslint \
-    gulp-typescript \
-    karma \
-    run-sequence \
-    tslint \
-    ts-node \
-    es6-module-loader \
-    jasmine-core \
-    karma-coverage \
-    karma-jasmine \
-    karma-mocha-reporter \
-    karma-phantomjs-launcher \
-    karma-chrome-launcher \
-    phantomjs-prebuilt \
-    systemjs \
-    traceur
+     awesome-typescript-loader \
+     chalk \
+     del \
+     es6-module-loader \
+     gulp \
+     gulp-inline-ng2-template \
+     gulp-load-plugins \
+     gulp-tap \
+     gulp-tslint \
+     gulp-typescript \
+     gulp-watch \
+     ionic-app-lib \
+     ionic-gulp-fonts-copy \
+     ionic-gulp-html-copy \
+     ionic-gulp-sass-build \
+     ionic-gulp-webpack-build \
+     jasmine-core \
+     karma \
+     karma-chrome-launcher \
+     karma-coverage \
+     karma-jasmine \
+     karma-mocha-reporter \
+     karma-phantomjs-launcher \
+     phantomjs-prebuilt \
+     run-sequence \
+     strip-sourcemap-loader \
+     systemjs \
+     traceur \
+     ts-node \
+     tslint \
+     typescript
 
 /bin/rm -fr tslint.json
 ./bin/tslint --init
